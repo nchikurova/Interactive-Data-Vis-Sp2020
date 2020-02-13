a2eee4835ee985e15a9f603970e3da7159803efa
Nadezda Chikurova <nadezdachikurova@Nadezdas-MacBook-Air.local>
2020-02-13 13:24:27 -0500

    k

--- tutorial2_quantities_and_amounts/main.js
+++ tutorial2_quantities_and_amounts/main.js
@@ -1,29 +1,40 @@
-d3.csv("../data/squirrelActivitis.csv", d3.autoType).then(data => {
+// data load
+// reference for d3.autotype: https://github.com/d3/d3-dsv#autoType
+d3.csv("../data/squirrelActivities.csv", d3.autoType).then(data => {
     console.log(data);
-    const width = window.innerWidth * 0.3,
+
+    /** CONSTANTS */
+    // constants help us reference the same values throughout our code
+    const width = window.innerWidth * 0.9,
         height = window.innerHeight / 2,
         paddingInner = 0.2,
-        margin = { top: 20, bottom: 40, left: 40, right: 40 };
+        margin = { top: 20, bottom: 40, left: 50, right: 40 };
 
-    /* Scales*/
+    /** SCALES */
+    // reference for d3.scales: https://github.com/d3/d3-scale
     const xScale = d3
-        .scaleBand()
+        .scaleLinear()
         .domain([0, d3.max(data, d => d.count)])
-        .range(height - margin.bottom, margin.top);
+        .range([margin.left, width - margin.right])
+
 
     const yScale = d3
-        .scaleLinear()
-        .domain(data.map(d => d.activity))
-        .range([margin.left, width - margin.right])
+        .scaleBand()
+        .domain([data, d => d.activity])
+        .range([0, height])
         .paddingInner(paddingInner);
 
-    const xAxis = d3.axisBottom(xScale).ticks(data.length);
 
+    // reference for d3.axis: https://github.com/d3/d3-axis
+    const yAxis = d3.axisLeft(yScale);
+    const xAxis = d3.axisBottom(xScale)
+
+    /** MAIN CODE */
     const svg = d3
-        .select("d3-container")
+        .select("#d3-container")
         .append("svg")
         .attr("width", width)
-        .attr("height", height)
+        .attr("height", height);
 
     // append rects
     const rect = svg
@@ -32,29 +43,30 @@ d3.csv("../data/squirrelActivitis.csv", d3.autoType).then(data => {
         .join("rect")
         .attr("y", d => yScale(d.activity))
         .attr("x", d => xScale(d.count))
-        .attr("width", d => width - margin.bottom - xScale(d.activity))
-        .attr("height", yScale.bandheight())
+        .attr("width", d => xScale(d.count))
+        .attr("height", yScale.bandwidth())
         .attr("fill", "steelblue")
 
-    // apend text
+    // append text
     const text = svg
         .selectAll("text")
         .data(data)
         .join("text")
         .attr("class", "label")
-        // this allow us to position the text in the center of the bar
-        .attr("x", d => xScale(d.count) + (xScale.bandheight() / 2))
-        .attr("y", d => yScale(d.activity))
-        .text(d => d.count)
+        // this allows us to position the text in the center of the bar
+        .attr("y", d => yScale(d.activity) + (yScale.bandwidth() / 2))
+        .attr("x", d => xScale(d.count))
+        .text(d => d.activity)
         .attr("dy", "1.25em");
+
     svg
         .append("g")
         .attr("class", "axis")
-        .attr("transform", 'translate(0, ${width - margin.bottom})')
+        .attr("transform", `translate(${margin.left},${margin.top})`)
+        .call(yAxis);
+    svg
+        .append("g")
+        .attr("class", "axis")
+        .attr("transform", 'translate(0, ${innerHeight})')
         .call(xAxis);
-
-
-
-
-
 });
