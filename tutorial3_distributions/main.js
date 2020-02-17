e4e3f11740dfbc892ad1089e4249012d98b75ee2
Nadezda Chikurova <nadezdachikurova@Nadezdas-MacBook-Air.local>
2020-02-17 16:41:21 -0500

    changes

--- tutorial3_distributions/main.js
+++ tutorial3_distributions/main.js
@@ -20,7 +20,7 @@ let state = {
 
 // Load data
 
-d3.json("../data/AirbnbD3.json", d3.autoType).then(raw_data => {
+d3.json("../data/AirbnbD3.csv", d3.autoType).then(raw_data => {
   console.log("raw_data", raw_data)
   state.data = "raw_data";
   init();
@@ -39,4 +39,123 @@ function init() {
   yScale = d3
     .scaleLinear()
     .domain(d3.extent(state.data, d => d.borough))
+    .range([height - margin.bottom, margin.top]);
+
+  // AXES
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale);
+
+  // UI element set up
+  // add dropdown (HTML selection) for interaction
+  // HTML select reference https://developer.mozilla.org/en-US/docs/Web/HTML/Element/select
+  const selectElement = d3.select("#dropdown").on("change", function () {
+    console.log("new selected room_type is", this.value);
+    //'this' === the selectElement
+    // this.value holds the dropdown value a user just selected
+    state.selectedType = this.value;
+    draw(); //re-draw the graph based on this new selection
+  })
+  // add in dropdown options from the unique values in the data
+  selectElement
+    .selectAll("option")
+    .data(["All", "Shared", "Private", "Entire"]) // unique data values-- (hint: to do this programmatically take a look `Sets`)
+    .join("option")
+    .attr("value", d => d)
+    .text(d => d);
+
+  // create an svg element in our main `d3-container` element
+  svg = d3
+    .select("#d3-container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  // add the xAxis
+  svg
+    .append("g")
+    .attr("class", "axis x-axis")
+    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .call(xAxis)
+    .append("text")
+    .attr("class", "axis-label")
+    .attr("x", "50%")
+    .attr("dy", "3em")
+    .text("Room Type");
+
+  // add the yAxis
+  svg
+    .append("g")
+    .attr("class", "axis y-axis")
+    .attr("transform", `translate(${margin.left},0)`)
+    .call(yAxis)
+    .append("text")
+    .attr("class", "axis-label")
+    .attr("y", "50%")
+    .attr("dx", "-3em")
+    .attr("writing-mode", "vertical-rl")
+    .text("Borough");
+
+  draw(); // calls the draw function
+}
+
+/**
+* DRAW FUNCTION
+* we call this everytime there is an update to the data/state
+* */
+function draw() {
+  // filter the data for the selectedParty
+  let filteredData = state.data;
+  // if there is a selectedParty, filter the data before mapping it to our elements
+  if (state.selectedType !== "All") {
+    filteredData = state.data.filter(d => d.party === state.selectedType);
+  }
+
+  const dot = svg
+    .selectAll(".dot")
+    .data(filteredData, d => d.name) // use `d.name` as the `key` to match between HTML and data elements
+    .join(
+      enter =>
+        // enter selections -- all data elements that don't have a `.dot` element attached to them yet
+        enter
+          .append("circle")
+          .attr("class", "dot") // Note: this is important so we can identify it in future updates
+          .attr("stroke", "lightgrey")
+          .attr("opacity", 0.5)
+          .attr("fill", d => {
+            if (d.party === "Shared") return "blue";
+            else if (d.party === "Private") return "teal";
+            else return "purple";
+          })
+          .attr("r", radius)
+          .attr("cy", d => yScale(d.borough))
+          .attr("cx", d => margin.left) // initial value - to be transitioned
+          .call(enter =>
+            enter
+              .transition() // initialize transition
+              .delay(d => 500 * d.room_type) // delay on each element
+              .duration(500) // duration 500ms
+              .attr("cx", d => xScale(d.room_type))
+          ),
+      update =>
+        update.call(update =>
+          // update selections -- all data elements that match with a `.dot` element
+          update
+            .transition()
+            .duration(250)
+            .attr("stroke", "black")
+            .transition()
+            .duration(250)
+            .attr("stroke", "lightgrey")
+        ),
+      exit =>
+        exit.call(exit =>
+          // exit selections -- all the `.dot` element that no longer match to HTML elements
+          exit
+            .transition()
+            .delay(d => 50 * d.room_type)
+            .duration(500)
+            .attr("cx", width)
+            .remove()
+        )
+    );
 }
